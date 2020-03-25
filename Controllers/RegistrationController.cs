13ecf84df9fdcca2d04238ead22df27a0df24da8
Brea Spencer <breaspencer@quickenloans.com>
2020-03-24 22:57:53 -0400

    first commit

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            ViewData["id"] = id;
+            return View("RegistrationIndex");
+        }
+        [HttpPost]
+        public IActionResult Registration(string fname, string lname, string email, int number, string password )
+        {
+            ViewData["name"] = fname + " " + lname;
+             return View();
+        }
+    }
+
+}
